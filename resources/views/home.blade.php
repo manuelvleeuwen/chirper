51c0e37f1ad89425465cbfb867a69c254ced370e
Manuel van Leeuwen <m.vanleeuwen12@student.han.nl>
2026-03-13 15:50:20 +0100

    Remove duplicate header and footer

--- resources/views/home.blade.php
+++ resources/views/home.blade.php
@@ -13,15 +13,6 @@
 </head>
 
 <body class="min-h-screen flex flex-col bg-base-200 font-sans">
-<nav class="navbar bg-base-100">
-    <div class="navbar-start">
-        <a href="/" class="btn btn-ghost text-xl">🐦 Chirper</a>
-    </div>
-    <div class="navbar-end gap-2">
-        <a href="#" class="btn btn-ghost btn-sm">Sign In</a>
-        <a href="#" class="btn btn-primary btn-sm">Sign Up</a>
-    </div>
-</nav>
 
 <main class="flex-1 container mx-auto px-4 py-8">
     <x-layout>
@@ -42,12 +33,6 @@
         </div>
     </x-layout>
 </main>
-
-<footer class="footer footer-center p-5 bg-base-300 text-base-content text-xs">
-    <div>
-        <p>© 2025 Chirper - Built with Laravel and ❤️</p>
-    </div>
-</footer>
 </body>
 
 </html>
